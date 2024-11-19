6a5f63d075d690e47cdae2e522a9b39264dd6cc5
mino <c0b231317b@edu.teu.ac.jp>
2024-11-19 17:07:15 +0900

    演習１：風に流される動き

--- pygame_image.py
+++ pygame_image.py
@@ -17,24 +17,31 @@ def main():
     tori_rct = tori_img.get_rect()
     tori_rct.center = 300, 200
     tmr = 0  #タイマー
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
         key_lst = pg.key.get_pressed() #8-3
         if key_lst[pg.K_UP]:
-            tori_rct.move_ip((0, -1))
+            tori_rct.move_ip((-1, -1))
         if key_lst[pg.K_DOWN]:
-            tori_rct.move_ip((0, 1))
+            tori_rct.move_ip((-1, 1))
         if key_lst[pg.K_LEFT]:
             tori_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            tori_rct.move_ip((1, 0))      
-
+            tori_rct.move_ip((1, 0)) 
+        else:
+            tori_rct.move_ip((-1, 0))
+        
+        
         x = -(tmr%3200)
+        #背景
         screen.blit(bg_img, [x, 0])  #読み込んだ画像を貼り付け(blit), 基本的にループの中
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
+        #こうかとん
         #screen.blit(tori_img, [300, 200])  #★４
         screen.blit(tori_img, tori_rct) #8-3
         
