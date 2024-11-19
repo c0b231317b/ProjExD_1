454798c38d03e9585059ec4231798e10f1e7dd32
mino <c0b231317b@edu.teu.ac.jp>
2024-11-19 16:38:09 +0900

    練習８：こうかとんに動きを付ける

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,31 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     tori_img = pg.image.load("fig/3.png")   #★２
     tori_img = pg.transform.flip(tori_img, True, False)
+    #★8
+    tori_rct = tori_img.get_rect()
+    tori_rct.center = 300, 200
     tmr = 0  #タイマー
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed() #8-3
+        if key_lst[pg.K_UP]:
+            tori_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            tori_rct.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            tori_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            tori_rct.move_ip((1, 0))      
+
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])  #読み込んだ画像を貼り付け(blit), 基本的にループの中
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
-        screen.blit(tori_img, [300, 200])  #★４
+        #screen.blit(tori_img, [300, 200])  #★４
+        screen.blit(tori_img, tori_rct) #8-3
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200)
